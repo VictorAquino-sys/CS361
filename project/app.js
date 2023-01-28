c7c6a699294b397f032721e458e72fb3af4e6dad
victor <aquinov@oregonstate.edu>
2023-01-27 18:24:35 -0800

    added files

--- project/app.js
+++ project/app.js
@@ -2,10 +2,8 @@
 const express = require('express');
 // import exphbs from 'express-handlebars';
 const exphbs = require('express-handlebars');
-// const db = require('./db');
 const app = express()
 
-// const { query } = require('express');
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 app.engine('.hbs', exphbs.engine({extname: ".hbs",defaultLayout:'main',layoutsDir: __dirname + '/views/layouts'}));  // Create an instance of the handlebars engine to process templates
 
@@ -32,7 +30,6 @@ app.use(function(req, res, next) {
 
 //Static Files
 app.use(express.static(__dirname + '/public')); //this is to serve our public folder as static
-// app.use(express.static(__dirname + '/server')); //this is to serve our public folder as static
 
 //API ROUTES
 
@@ -43,22 +40,39 @@ app.get('/', (req,res)=>{
 app.get('/results', (req,res)=>{
 
     db.con.query('SELECT * FROM product', function(error, rows, fields){
-    // Save the products info
-    // let people = rows;
     return res.render('results', {data: rows});
     
     })
 
-
-    // res.sendFile(__dirname + '/public/results.php');
 })
 
 app.post('/results', async (req, res) => {
     let data = req.body;
-    console.log(data);
-    const channelData = await scrapers.scrapeChannel(data)
+    const channelData = await scrapers.scrapeChannelAMAZON(data)
+    // const channelData2 = await scrapers.scrapeChannelWALMART(data)
     res.redirect('/results');
 
 })
+
+app.post('/delete_product', function(req, res)
+{
+    let data = req.body;
+
+    query1 = `DELETE FROM product WHERE id = '${data['delete-name_product']}'`;
+    db.con.query(query1,function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else
+        {
+            res.redirect('/results');
+        }
+    })
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
--- project/scrapers.js
+++ project/scrapers.js
@@ -1,8 +1,9 @@
 const puppeteer = require('puppeteer');
 const db = require('./db');
 
-async function scrapeChannel(url) {
-    var receiveurl = JSON.stringify(url);   
+// scraping for AMAZON
+async function scrapeChannelAMAZON(productname) {
+    var receiveurl = JSON.stringify(productname);   
     console.log('test: ' + receiveurl);
     var channelURL = receiveurl.replace(/ /g, '+');
     var userURL = `https://www.amazon.com/s?k=${channelURL}&sprefix=samsubng%2Caps%2C166&ref=nb_sb_ss_pltr-ranker-lnopsacceptance_1_8`
@@ -30,14 +31,6 @@ async function scrapeChannel(url) {
 
     browser.close();
 
-    // for(let i = 0; i < 6; i++){
-    //     console.log('Title: ' , title[i]);
-    //     console.log('Price: ' , price[i]);
-    //     console.log('Score: ' , score[i]);
-    //     console.log('URL: ' , weburl[i]);
-    //     console.log(' ');
-    // }
-
     for(let i = 0; i < 5; i++){
 
         const insertCompanyList = "INSERT INTO `product` ( `title`, `price`, `score`, `url` ) VALUES (?,?,?,?)";
@@ -49,13 +42,63 @@ async function scrapeChannel(url) {
               console.log('DB insert successful. Record: ' +  i);
             }
         });
-        //   console.log(insertsql);
     }
-    // return {title, price, score, weburl}
 }
+
+// scraping for WALMART
+// async function scrapeChannelWALMART(productname) {
+//     var receiveurl = JSON.stringify(Object.values(productname));  
+//     // var receiveurl = object.values(productname);
+//     var channelURL = receiveurl.replace(/ /g, '+');
+//     var channelURL = channelURL.replace(/"/g, '');
+//     var channelURL = channelURL.replace(/[\[\]']+/g, ''); //remove square brackets
+//     // console.log('test: ' + channelURL);
+//     // console.log('check product name walmart: ' + channelURL)
+//     var userURL = `https://www.walmart.com/search?q=${channelURL}`
+//     console.log('testuserURL: ' + userURL)
+
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto(userURL);
+
+//     const title = await page.evaluate(() => {
+//         return Array.from(document.querySelectorAll(".normal.dark-gray.mb0.mt1.lh-title.f6.f5-l")).map(x=>x.textContent);
+//     });
+
+//     console.log('check title ' + title);
+
+//     const price = await page.evaluate(() => {
+//         return Array.from(document.querySelectorAll(".a-offscreen")).map(x=>x.textContent);
+//     });
+
+//     const score = await page.evaluate(() => {
+//         return Array.from(document.querySelectorAll(".a-icon-alt")).map(x=>x.textContent);
+//     });
+
+ 
+//     const weburl = await page.evaluate(() => {
+//         return Array.from(document.querySelectorAll('a[href]'), a => a.getAttribute('href'));
+//     });
+
+//     browser.close();
+
+//     for(let i = 0; i < 5; i++){
+
+//         const insertCompanyList = "INSERT INTO `product` ( `title`, `price`, `score`, `url` ) VALUES (?,?,?,?)";
+
+//         const insertsql = db.con.query(insertCompanyList, [title[i], price[i], score[i], weburl[i]], function(err, rows) {
+//             if (err) {
+//               console.log(err);
+//             } else {
+//               console.log('DB insert successful. Record: ' +  i);
+//             }
+//         });
+//     }
+// }
 
 // scrapeChannel('https://www.amazon.com/s?k=alexa+echo&sprefix=alexa%2Caps%2C168&ref=nb_sb_ss_pltr-ranker-lnopsacceptance_4_5')
 
 module.exports = {
-    scrapeChannel
+    scrapeChannelAMAZON
+    // scrapeChannelWALMART
 }
